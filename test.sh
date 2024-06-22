c2e0962e65897a53ea3946515829644a264d99ba
saraAbdulla23 <sarahkhalaf23012@gmail.com>
2024-06-22 20:45:41 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+./push-swap
+
+./push-swap "2 1 3 6 5 8".
+
+./push-swap "0 1 2 3 4 5"
+
+./push-swap "0 one 2 3"
+
+./push-swap "1 2 2 3"
+
+./checker
+
+./checker "0 one 2 3"
+
+echo -e "sa\npb\nrrr\n" | ./checker "0 9 1 8 2 7 3 6 4 5"
+
+echo -e "pb\nra\npb\nra\nsa\nra\npa\npa\n" | ./checker "0 9 1 8 2"
+
+ARG="4 67 3 87 23"; ./push-swap "$ARG" | ./checker "$ARG"
+
+ARG="<100 random numbers>"; ./push-swap "$ARG"
+
+ARG="<100 random numbers>"; ./push-swap "$ARG" | ./checker "$ARG"
